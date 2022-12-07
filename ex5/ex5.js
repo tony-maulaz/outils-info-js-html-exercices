6d2cde8859d08f918332fe6e0fe02fbf277f645c
Maulaz Tony <tony.maulaz@heig-vd.ch>
2022-12-07 11:10:26 +0100

    Create solution and student version

--- ex5/ex5.js
+++ ex5/ex5.js
@@ -1,17 +1,4 @@
 
 function addClass(button){
-    const div = document.getElementById("div1")
-    window.test = div
 
-    const className = "c3"
-    if (div.classList.contains(className)){
-        div.classList.remove(className)
-        button.innerHTML = "Add class"
-    }
-    else{
-        div.classList.add(className)
-        button.innerHTML = "Remove class"
-    }
-
-    document.getElementById("class_list").innerHTML = div.classList
 }
